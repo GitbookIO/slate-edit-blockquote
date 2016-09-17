f7063fb8eafe826e97c79ba20f247681cf15c16c
Samy Pesse <samypesse@gmail.com>
2016-09-17 10:49:38 +0200

    Base code

--- README.md
+++ README.md
@@ -1,2 +1,44 @@
 # slate-edit-blockquote
-A Slate plugin to handle keyboard events in blockquotes
+
+[![NPM version](https://badge.fury.io/js/slate-edit-blockquote.svg)](http://badge.fury.io/js/slate-edit-blockquote)
+[![Linux Build Status](https://travis-ci.org/GitbookIO/slate-edit-blockquote.png?branch=master)](https://travis-ci.org/GitbookIO/slate-edit-blockquote)
+
+A Slate plugin to handle keyboard events in blockquotes. Blockquotes can contain blocks.
+
+### Install
+
+```
+npm install slate-edit-blockquote
+```
+
+### Features
+
+Natural keybindings:
+
+- Pressing <kbd>Enter</kbd> twice exit the blockquote
+
+### Simple Usage
+
+```js
+import EditBlockquote from 'slate-edit-blockquote'
+
+const plugins = [
+  EditBlockquote()
+]
+```
+
+#### Arguments
+
+This plugin accepts options to redefine the following block types:
+
+- ``[type: String]`` — type for blockquotes
+- ``[typeDefault: String]`` — type for default block in blockquote.
+
+### Utilities
+
+
+#### `transforms.wrapInBlockquote`
+
+`plugin.transforms.wrapInBlockquote(transform: Transform) => Transform`
+
+Wrap current block in a new blockquote.
---
+++ tests/all.js
@@ -0,0 +1,40 @@
+const expect = require('expect');
+const fs = require('fs');
+const path = require('path');
+const Slate = require('slate');
+const readMetadata = require('read-metadata');
+
+const EditList = require('../lib');
+
+describe('slate-edit-blockquote', function() {
+    const tests = fs.readdirSync(__dirname);
+    const plugin = EditList();
+
+    tests.forEach(function(test) {
+        if (test[0] === '.' || path.extname(test).length > 0) return;
+
+        it(test, function() {
+            const dir = path.resolve(__dirname, test);
+
+            const inputPath = path.resolve(dir, 'input.yaml');
+            const input = readMetadata.sync(inputPath);
+
+            const expectedPath = path.resolve(dir, 'expected.yaml');
+            let expected;
+            if (fs.existsSync(expectedPath)) {
+                expected = readMetadata.sync(expectedPath);
+            }
+
+            const runTransform = require(path.resolve(dir, 'transform.js'));
+
+            const stateInput = Slate.Raw.deserialize(input, { terse: true });
+
+            const newState = runTransform(plugin, stateInput);
+
+            if (expected) {
+                const newDocJSon = Slate.Raw.serialize(newState, { terse: true });
+                expect(newDocJSon).toEqual(expected);
+            }
+        });
+    });
+});
